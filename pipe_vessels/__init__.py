dad9268a69d587cf63f7a73c3b24a37846f4bf2c
Enrique Tuya <enriquetuya@gmail.com>
2019-08-12 18:01:17 -0300

    Increase the version.

--- pipe_vessels/__init__.py
+++ pipe_vessels/__init__.py
@@ -2,7 +2,7 @@
 Pipeline for extracting and compiling vessel information
 """
 
-__version__ = '1.0.0'
+__version__ = '1.1.0'
 __author__ = 'Andres Arana'
 __email__ = 'andres@globalfishingwatch.org'
 __source__ = 'https://github.com/GlobalFishingWatch/pipe-vessels'
